1c286fc88364daad44db78e16ee53e71f7466e7a
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2024-03-06 15:04:07 -0300

    feat: register user into database

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -12,6 +12,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -47,7 +48,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        username: data.username,
+        name: data.name,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
